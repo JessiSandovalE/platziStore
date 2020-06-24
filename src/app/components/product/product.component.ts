9b31695a1a4e521b1b3668ccfe8b9d9ef9d5448d
jessica <jessi.sandoval93@gmail.com>
2020-06-23 19:55:38 -0500

    first commit

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../../product.model';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent {
+
+  @Input() product: Product;
+  @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+
+
+  addCart() {
+    console.log('Añadir al carrito')
+    this.productClicked.emit(this.product.id);
+  }
+}
