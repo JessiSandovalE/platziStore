86753de3653838026b13aa6a2454feaffb9c00d9
jessica <jessi.sandoval93@gmail.com>
2020-07-01 23:47:07 -0500

    banner con swiper

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  mySwiper: Swiper;
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngAfterViewInit() {
+    this.mySwiper = new Swiper('.swiper-container');
   }
 
 }
